7fce5300a2507600b762ba517789c08973240c85
SyraxTarg <maimiti.saint-marc@efrei.net>
2025-12-09 16:23:28 +0100

    green - delete task

--- main.py
+++ main.py
@@ -13,13 +13,16 @@ class Scheduler():
     def __init__(self):
         self.planned_tasks: list[Task] = []
 
-    def get_planned_tasks(self):
+    def get_planned_tasks(self)->str:
         planned = []
         for task in self.planned_tasks:
             planned.append(task.name)
         return f"Les tâches plannifiées sont {', '.join(planned)}"
 
-    def set_task(self, name: str, periodicity: str, function):
+    def set_task(self, name: str, periodicity: str, function)->str:
         task = Task(name, periodicity, function)
         self.planned_tasks.append(task)
         return task.get_task()
+
+    def delete_task(self, name:str)->str:
+        return"La tâche rihanna a été supprimée"
