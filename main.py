39cc170eb128c71793c03f9951dc5c2a59e33796
SyraxTarg <maimiti.saint-marc@efrei.net>
2025-12-09 15:51:56 +0100

    green/refacto - task class

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+class Task():
+
+    def __init__(self, name: str, periodicity: str, function):
+        self.name = name
+        self.periodicity = periodicity
+        self.function = function
+
+    def get_task(self):
+        return f"La tâche {self.name} avec une périodicité de {self.periodicity}"
+
 class Scheduler():
-    
+
     def __init__(self):
         self.running_tasks = []
