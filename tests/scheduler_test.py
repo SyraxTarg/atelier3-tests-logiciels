2dc85000432645ee175c184ec5bc824a75386abc
SyraxTarg <maimiti.saint-marc@efrei.net>
2025-12-09 15:56:03 +0100

    red - scheduler getter

--- tests/scheduler_test.py
+++ tests/scheduler_test.py
@@ -2,7 +2,7 @@ from main import Scheduler, Task
 
 def test_init_scheduler():
     s = Scheduler()
-    assert s.running_tasks == []
+    assert s.planned_tasks == []
 
 def test_init_task():
     t = Task("toto", "0 0 13 * 5", lambda x: print(f"Salut {x}"))
@@ -20,3 +20,16 @@ def test_get_task():
 
     # Assert
     assert result == "La tâche toto avec une périodicité de 0 0 13 * 5"
+    
+def test_get_planned_tasks():
+    # Arrange
+    s = Scheduler()
+    t1 = Task("yoyo", "0 0 23 * 4", lambda x: print(f"Salut {x}"))
+    t2 = Task("toto", "0 0 13 * 5", lambda x: print(f"Salut {x}"))
+    s.planned_tasks = [t1, t2]
+    
+    # Act
+    result = s.get_planned_tasks()
+    
+    # Assert
+    assert result == f"Les tâches plannifiées sont t1, t2"
